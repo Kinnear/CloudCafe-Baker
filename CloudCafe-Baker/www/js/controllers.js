d351629e971751902e20179e0c6fd5be6d35b480
Kinnear <Kinnear>
2016-03-04 00:32:54 +0800

    Decided to use Facebook authentication for now exclusively. will try to make it extensible for other types of authentications.

--- CloudCafe-Baker/www/js/controllers.js
+++ CloudCafe-Baker/www/js/controllers.js
@@ -9,9 +9,9 @@ app.controller("FacebookAuthentication", function($scope, CurrentUserData){
     var ref = new Firebase(firebaseURL);
     ref.onAuth(authDataCallback);
 
-    $scope.LoginFacebook = function()
+    $scope.LoginFacebook = function(value)
     {
-        ref.authWithOAuthPopup("facebook", function(error, authData) {
+        ref.authWithOAuthPopup(value, function(error, authData) {
             if (error) {
                 console.log("Login Failed!", error);
                 // $scope.loggedIn = false;
@@ -24,7 +24,7 @@ app.controller("FacebookAuthentication", function($scope, CurrentUserData){
         });
     }
 
-    $scope.LogoutFacebook = function()
+    $scope.LogoutAuthentication = function()
     {
         ref.unauth();
         CurrentUserData.clearAuthenticationData();
@@ -48,11 +48,54 @@ app.controller("FacebookAuthentication", function($scope, CurrentUserData){
         }
         else 
         {
-            // $scope.loggedIn = false;
             console.log("User is logged out");
         }
     }
 });
+
+
+// // we would probably save a profile when we register new users on our site
+// // we could also read the profile to see if it's null
+// // here we will just simulate this with an isNewUser boolean
+// var isNewUser = true;
+// var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+// ref.onAuth(function(authData) {
+//   if (authData && isNewUser) {
+//     // save the user's profile into the database so we can list users,
+//     // use them in Security and Firebase Rules, and show profiles
+//     ref.child("users").child(authData.uid).set({
+//       provider: authData.provider,
+//       name: getName(authData)
+//     });
+//   }
+// });
+// // find a suitable name based on the meta info given by each provider
+// function getName(authData) {
+//   switch(authData.provider) {
+//      case 'password':
+//        return authData.password.email.replace(/@.*/, '');
+//      case 'twitter':
+//        return authData.twitter.displayName;
+//      case 'facebook':
+//        return authData.facebook.displayName;
+//   }
+// }
+// {
+//   "users": {
+//     "c20f3bea-906f-4cd6-b38a-175bb4506287": {
+//       "provider": "password",
+//       "name": "bobtony"
+//     },
+//     "1376ecee-fc04-4809-9de9-ff41747e12a7": {
+//       "provider": "twitter",
+//       "name": "Andrew Lee"
+//     },
+//     "9550be91-ab5d-41c1-8cd0-24c883586071": {
+//       "provider": "facebook",
+//       "name": "James Tamplin"
+//     }
+//   }
+// }
 
 app.controller("AddUser", function($scope, GetAll){
    
@@ -61,44 +104,19 @@ app.controller("AddUser", function($scope, GetAll){
     
    $scope.AddUser = function()
    {
-    //  console.log($scope.form.categoryID.$id);
+        // console.log($scope.form.categoryID.$id);
         // console.log("$scope.img4URI" + $scope.img4URI);
-        
-//         {
-// users:
-// {
-// userID:{
-// name: "Kinnear",
-// baker: true
-// }
-// }
-// }
-        
-       
-        temp.set({  
-                            "users":{
-                                "userID":
-                                {
-                                    "username": "Kinnear",
-                                    "baker": true,
-                                    "stallID": 18821912
-                                }
-                            }
-            
-                            });
-            
-                            //     "categoryID" : $scope.form.categoryID.$id,
-                            //     "description" : $scope.form.description,
-                            //     "foodName": $scope.form.foodName,
-                            //     "halal": $scope.form.halal,
-                            //     "img1": $scope.img1URI,
-                            //     "img2": $scope.img2URI,
-                            //     "img3": $scope.img3URI,
-                            //     "img4": $scope.img4URI,
-                            //     "likes": 0,
-                            //     "price": $scope.form.price,
-                            //     "stallID": $scope.form.stallID
-                            // });
+        // temp.set({  
+        //             "users":{
+        //                 "userID":
+        //                 {
+        //                     "username": "Kinnear",
+        //                     "baker": true,
+        //                     "stallID": 18821912
+        //                 }
+        //             }
+    
+        //          });
    }
 });
 
