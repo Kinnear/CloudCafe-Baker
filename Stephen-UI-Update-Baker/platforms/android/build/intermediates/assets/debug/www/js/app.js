e319646ef462297b8ad6242c51a70dfed04a51ab
Kinnear <Kinnear>
2016-06-01 23:10:07 +0800

    [Improvement] Ensures that at least one picture has to be uploaded when registering or adding new food  			 Removed max quantity field, and now set it to a default 0 quantity

--- Stephen-UI-Update-Baker/platforms/android/build/intermediates/assets/debug/www/js/app.js
+++ Stephen-UI-Update-Baker/platforms/android/build/intermediates/assets/debug/www/js/app.js
@@ -7,7 +7,7 @@ var STRIPE_API_PUBLISHABLE_KEY = "pk_test_h57hQy5dRjVjlM7SoNVYG8Mn";
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('starter', ['ionic', "ngMessages", 'ui.router', 'stripe.checkout', 'starter.controllers', 'starter.services', 'nl2br', 'monospaced.elastic', "ngCordova", 'ionic-native-transitions']);
+var app = angular.module('starter', ['ionic', "ngMessages", 'ui.router', 'stripe.checkout', 'starter.controllers', 'starter.services', 'nl2br', 'monospaced.elastic', 'ngCordova', 'ionic-native-transitions']);
 
 app.run(["$rootScope", "$state", "$ionicPlatform", "$timeout", function ($rootScope, $state, $ionicPlatform, $timeout) {
 
